7b771cabe24b57caff13ebe0bbc1905800010f48
soumik12345 <19soumik.rakshit96@gmail.com>
2025-05-01 19:31:03 +0000

    add: tests

---
+++ tests/test_reid_dataset.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+from firerequests import FireRequests
+
+from trackers.core.reid import Market1501SiameseDataset
+from trackers.utils.data_utils import unzip_file
+
+DATASET_URL = "https://storage.googleapis.com/com-roboflow-marketing/trackers/datasets/market_1501.zip"
+
+def test_reid_dataset():
+    FireRequests().download(DATASET_URL)
+    os.makedirs("test_data", exist_ok=True)
+    shutil.move("market_1501.zip", "test_data/market_1501.zip")
+    unzip_file("test_data/market_1501.zip", "test_data")
+    os.remove("test_data/market_1501.zip")
+    dataset = Market1501SiameseDataset("./test_data/Market-1501-v15.09.15")
+    assert len(dataset) == 751
+    shutil.rmtree("test_data")
