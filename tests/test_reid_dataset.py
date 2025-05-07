179884fd59db5421383dd676fc4ef4156447d9e5
soumik12345 <19soumik.rakshit96@gmail.com>
2025-05-07 21:51:19 +0530

    update: tests

--- tests/test_reid_dataset.py
+++ tests/test_reid_dataset.py
@@ -23,9 +23,9 @@ def market_dataset():
     yield dataset_path
 
 
-def test_reid_dataset(market_dataset):
+def test_market1501_dataset_train_val_split(market_dataset):
     train_dataset, val_dataset = get_market1501_dataset(
-        os.path.join(market_dataset, "bounding_box_train")
+        os.path.join(market_dataset, "bounding_box_train"), split_ratio=0.8
     )
     if not len(train_dataset) == 600:  # nosec B101
         pytest.fail(f"Dataset length mismatch. Expected 751, got {len(train_dataset)}")
@@ -53,6 +53,31 @@ def test_reid_dataset(market_dataset):
     for tracker_id in val_dataset.tracker_ids:
         anchor_image_path, positive_image_path, negative_image_path = (
             val_dataset._get_triplet_image_paths(tracker_id)
+        )
+        anchor_image_id = os.path.basename(anchor_image_path).split("_")[0]
+        positive_image_id = os.path.basename(positive_image_path).split("_")[0]
+        negative_image_id = os.path.basename(negative_image_path).split("_")[0]
+        if anchor_image_id != positive_image_id:
+            pytest.fail(
+                "Anchor and positive image IDs mismatch. "
+                f"Expected {anchor_image_id} == {positive_image_id}"
+            )
+        if anchor_image_id == negative_image_id:
+            pytest.fail(
+                "Anchor and negative image IDs mismatch. "
+                f"Expected {anchor_image_id} != {negative_image_id}"
+            )
+
+
+def test_market1501_dataset_test_split(market_dataset):
+    test_dataset = get_market1501_dataset(
+        os.path.join(market_dataset, "bounding_box_test"), split_ratio=None
+    )
+    if not len(test_dataset) == 752:  # nosec B101
+        pytest.fail(f"Dataset length mismatch. Expected 752, got {len(test_dataset)}")
+    for tracker_id in test_dataset.tracker_ids:
+        anchor_image_path, positive_image_path, negative_image_path = (
+            test_dataset._get_triplet_image_paths(tracker_id)
         )
         anchor_image_id = os.path.basename(anchor_image_path).split("_")[0]
         positive_image_id = os.path.basename(positive_image_path).split("_")[0]
