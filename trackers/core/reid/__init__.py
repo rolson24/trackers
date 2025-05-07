82b48c3c81649ca6900d1bced4bcd91968eedda6
soumik12345 <19soumik.rakshit96@gmail.com>
2025-05-07 18:26:15 +0530

    chore: address feedack

--- trackers/core/reid/__init__.py
+++ trackers/core/reid/__init__.py
@@ -1,3 +1,4 @@
+from trackers.core.reid.data.base import TripletsDataset
 from trackers.core.reid.data.market_1501 import get_market1501_dataset
 
-__all__ = ["get_market1501_dataset"]
+__all__ = ["TripletsDataset", "get_market1501_dataset"]
