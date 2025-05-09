bfa53eb3c9861e512df97d9ed5d519946a5145a0
Onuralp SEZER <thunderbirdtr@gmail.com>
2025-05-09 17:41:34 +0300

    feat: 🚀 implement AsyncFileDownloader for efficient file downloads with progress tracking
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- trackers/core/deepsort/feature_extractor.py
+++ trackers/core/deepsort/feature_extractor.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import Optional, Tuple, Union
 
 import numpy as np
@@ -6,7 +7,6 @@ import timm
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
-from firerequests import FireRequests
 
 from trackers.utils.torch_utils import parse_device_spec
 
@@ -112,7 +112,28 @@ class DeepSORTFeatureExtractor:
             import validators
 
             if validators.url(model_or_checkpoint_path):
-                checkpoint_path = FireRequests().download(model_or_checkpoint_path)[0]
+                from trackers.utils.download_file_utils import AsyncFileDownloader
+
+                downloader = AsyncFileDownloader()
+                try:
+                    loop = asyncio.get_event_loop()
+                    if loop.is_running():
+                        future = asyncio.ensure_future(
+                            downloader.process_url(model_or_checkpoint_path)
+                        )
+                        checkpoint_path = loop.run_until_complete(future)
+                    else:
+                        checkpoint_path = loop.run_until_complete(
+                            downloader.process_url(model_or_checkpoint_path)
+                        )
+                except RuntimeError:
+                    checkpoint_path = asyncio.run(
+                        downloader.process_url(model_or_checkpoint_path)
+                    )
+                print(
+                    f"Model downloaded to {checkpoint_path} "
+                    "Loading the model from the checkpoint."
+                )
                 self._load_model_from_path(checkpoint_path)
             else:
                 self._load_model_from_path(model_or_checkpoint_path)
