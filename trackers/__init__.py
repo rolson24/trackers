0b212ea7b58bbddb96a087907f78e1c0f3c3f397
soumik12345 <19soumik.rakshit96@gmail.com>
2025-05-12 11:56:11 +0530

    chore: publicly expose ReIDModel

--- trackers/__init__.py
+++ trackers/__init__.py
@@ -1,8 +1,7 @@
-from trackers.core.reid import ReIDModel
 from trackers.core.sort.tracker import SORTTracker
 from trackers.log import get_logger
 
-__all__ = ["ReIDModel", "SORTTracker"]
+__all__ = ["SORTTracker"]
 
 logger = get_logger(__name__)
 
@@ -15,5 +14,17 @@ except ImportError:
     logger.warning(
         "DeepSORT dependencies not installed. DeepSORT features will not be available. "
         "Please run `pip install trackers[deepsort]` and try again."
+    )
+    pass
+
+
+try:
+    from trackers.core.reid.model import ReIDModel
+
+    __all__.append("ReIDModel")
+except ImportError:
+    logger.warning(
+        "ReIDModel dependencies not installed. ReIDModel will not be available. "
+        "Please run `pip install trackers[reid]` and try again."
     )
     pass
