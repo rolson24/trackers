a3d77390672e658484ced4aaaeb82fb382d88bfc
soumik12345 <19soumik.rakshit96@gmail.com>
2025-05-14 16:29:07 +0530

    chore: address feedback on reid imports

--- trackers/core/reid/__init__.py
+++ trackers/core/reid/__init__.py
@@ -1,5 +1,16 @@
-from trackers.core.reid.dataset.base import TripletsDataset
-from trackers.core.reid.dataset.market_1501 import get_market1501_dataset
-from trackers.core.reid.model import ReIDModel
+from trackers.log import get_logger
 
-__all__ = ["ReIDModel", "TripletsDataset", "get_market1501_dataset"]
+logger = get_logger(__name__)
+
+try:
+    from trackers.core.reid.dataset.base import TripletsDataset
+    from trackers.core.reid.dataset.market_1501 import get_market1501_dataset
+    from trackers.core.reid.model import ReIDModel
+
+    __all__ = ["ReIDModel", "TripletsDataset", "get_market1501_dataset"]
+except ImportError:
+    logger.warning(
+        "ReIDModel dependencies not installed. ReIDModel will not be available. "
+        "Please run `pip install trackers[reid]` and try again."
+    )
+    pass
